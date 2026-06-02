835cfd46646045be6c39f9cb42119ac0aa883414
univille <univille@LAB-SIS-NOTE-11.univille.br>
2026-06-02 20:19:02 -0300

    segundo commit

--- config/database.php
+++ config/database.php
@@ -6,10 +6,11 @@ $user = 'root';
 $password = '';
 try {
  $pdo = new PDO(
- "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+ "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
  $user,
  $password
  );
+ echo"ok";
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
  die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
