20b9927ac720d19407d6dab130485c92830878f8
GUSTAVO HENRIQUE SCHUMACHER <gustavo.schumacher@univille.br>
2026-06-02 19:49:33 -0300

    primeira edição

--- config/database.php
+++ config/database.php
@@ -5,13 +5,12 @@ $dbname = 'atendelab';
 $user = 'root';
 $password = '';
 try {
- $pdo = new PDO(
- "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
- $user,
- $password
- );
- $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
- die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+    die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
 }
-13. Test
