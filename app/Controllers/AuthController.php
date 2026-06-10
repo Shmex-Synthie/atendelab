a7ca4e574cc2d884d32da9524bc0830b40e7c3f4
frazzonn <143911231+frazzonn@users.noreply.github.com>
2026-06-10 20:14:30 -0300

    Add files via upload

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../Middleware/auth.php';
 class AuthController
 {
 
-    private PDO %pdo;
+    private PDO $pdo;
 
     public function __construct()
     {
@@ -111,32 +111,44 @@ class AuthController
     }
 
 
+    //public function logout(): void
+    //{
+    //    $_SESSION = [];
+
+    //    if (ini_get('session.use_cookies')) {
+    //        $params = session_get_cookie_params();
+
+    //        setcookie(
+    //            session_name(),
+    //            '',
+    //            time() - 42000,
+    //            $params['path'],
+    //            $params['domain'],
+    //            $params['secure'],
+    //            $params['httponly']
+    //        );
+    //    }
+
+    //    session_destroy();
+
+    //    session_start();
+
+    //    $_SESSION['mensagem'] = 'Sessão encerrada com sucesso.';
+
+    //    header('Location: ?controller=auth&action=login');
+    //    exit;
+    //}
+
     public function logout(): void
-    {
-        $_SESSION = [];
+{
+    unset($_SESSION['usuario']);
 
-        if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
+    $_SESSION = [];
 
-            setcookie(
-                session_name(),
-                '',
-                time() - 42000,
-                $params['path'],
-                $params['domain'],
-                $params['secure'],
-                $params['httponly']
-            );
-        }
+    session_destroy();
 
-        session_destroy();
-
-        session_start();
-
-        $_SESSION['mensagem'] = 'Sessão encerrada com sucesso.';
-
-        header('Location: ?controller=auth&action=login');
-        exit;
-    }
+    header('Location: ?controller=auth&action=login');
+    exit;
+}
     
 }
